8b75e905b7601464539392c2feb2aaade568ff73
Douglas Hellinger <doug.hellinger@hotmail.com>
2026-07-16 12:04:29 +0800

    chore: add remaining tracked config files
    Add .config/fish/config.fish, .devcontainer.json,
    .fonts/LICENSE.md, .fonts/readme.md to match expected tree.

---
+++ .config/fish/config.fish
@@ -0,0 +1,6 @@
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+eval /home/dough/micromamba/bin/conda "shell.fish" "hook" $argv | source
+# <<< conda initialize <<<
+
